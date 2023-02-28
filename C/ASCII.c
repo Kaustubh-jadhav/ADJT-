5827f8820702a32fcd56b51091925aa85c153eac
Kaustubh madhav jadhav <63606341+Champion028K@users.noreply.github.com>
2023-02-28 21:34:13 +0530

    charactor to ASCII code is added

---
+++ C/ASCII.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char temp;
+    printf("\n Enter :");
+    scanf("%c",&temp);
+    printf("\nASCII value : %d",temp);
+    return 0;
+}
--- C/pattern1.c
+++ C/pattern1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main() {
+/*int main() {
   int i, j, k = 9, t = 2;
   for (i = 0; i < 5; i++) {
     k = 9;
@@ -12,10 +12,34 @@ int main() {
     printf("\n");
   }
   return 0;
-}
+}*/
 /*
 9
 97
 974
 9740
 */
+#define MAX 5
+int main()
+{
+  int a[MAX],i,j,temp;
+  printf("Enter 5 numbers :");
+  for(i=0;i<MAX;i++)
+    scanf("%d",&a[i]);
+  for(i=0;i<MAX;i++)
+  {
+    for(j=i+1;j<MAX;j++)
+    {
+      if(a[i]<a[j])
+      {
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+      }
+    }
+  }
+  printf("array in dscending order :");
+  for(i=0;i<MAX;i++)
+     printf("%3d",a[i]);
+  return 0;
+}
